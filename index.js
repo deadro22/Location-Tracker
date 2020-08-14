d80cfa52696254de98f06598a3c7ccb7bb633474
deadro22 <44213280+deadro22@users.noreply.github.com>
2020-08-14 23:52:19 +0100

    ch

--- index.js
+++ index.js
@@ -41,6 +41,6 @@ app.post("/newLocation", async (req, res) => {
   }
 });
 
-app.listen(process.env.port || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port 3000");
 });
